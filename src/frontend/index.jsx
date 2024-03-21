f5dc6c30fe94f36089ba58142fe64b87243ff38c
Thomas Manley <tmanley1985@gmail.com>
2024-03-21 13:26:00 -0400

    Setting up react, mobx, parcel, etc.

---
+++ src/frontend/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import rental from "../ch03/rental-AST"
+
+require("./styling.css")
+
+createRoot(document.getElementById("root")).render(
+  <span>Frontend goes here!</span>
+)
